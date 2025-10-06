bc07039266684f5b471fb9c5829a14c2c0b810be
AxelelPro17 <amorales@siconta.com>
2025-10-05 21:40:36 -0600

    24

--- app.js
+++ app.js
@@ -1,99 +1,3 @@
-let cantidadNoticias = 5;
-let pageFinal = cantidadNoticias;
-let pageInicial = 0;
-let temaActual = "Music";
-
-let noticias = {
-  "apiKey":"f3798e116eb342b2bae58e7f0cbd9c11",
-  fetchNoticias:function(temaActual){
-    fetch(
-      "https://newsapi.org/v2/everything?q="
-      +temaActual+
-      "&language=es&apiKey="+this.apiKey
-    )
-    .then((response)=>response.json())
-    .then((data)=>this.displayNoticias(data));
-  },
-  displayNoticias: function(data){
-    //elimino todo si ha seleccionado un nuevo tema
-    if(pageInicial==0){
-      document.querySelector(".container-noticias").textContent ="";
-    }
-
-    for(i=pageInicial;i<=pageFinal;i++){
-      const {title} = data.articles[i];
-      let h2 = document.createElement("h2");
-      h2.textContent = title;
-    
-      const {urlToImage} = data.articles[i];
-      let img = document.createElement("img");
-      img.setAttribute("src", urlToImage);
-
-      let info_item = document.createElement("div");
-      info_item.className = "info_item";
-      const {publishedAt} = data.articles[i];
-      let fecha = document.createElement("span");
-      let date = publishedAt;
-      date=date.split("T")[0].split("-").reverse().join("-");
-      fecha.className = "fecha";
-      fecha.textContent = date;
-
-      const {name} = data.articles[i].source;
-      let fuente = document.createElement("span");
-      fuente.className = "fuente";
-      fuente.textContent = name;
-
-      info_item.appendChild(fecha);
-      info_item.appendChild(fuente);
-
-      const {url} = data.articles[i];
-
-      let item = document.createElement("div");
-      item.className = "item";
-      item.appendChild(h2);
-      item.appendChild(img);
-      item.appendChild(info_item);
-      item.setAttribute("onclick", "location.href='"+url+"'");
-      document.querySelector(".container-noticias").appendChild(item);
-    }
-
-    let btnSiguiente = document.createElement("span");
-    btnSiguiente.id = "btnSiguiente";
-    btnSiguiente.textContent = "Ver más";
-    btnSiguiente.setAttribute("onclick","siguiente()");
-    document.querySelector(".container-noticias").appendChild(btnSiguiente);
-  }
-}
-
-
-function buscar(cat){
-  pageInicial = 0;
-  pageFinal = cantidadNoticias;
-  temaActual = cat;
-  noticias.fetchNoticias(cat);
-}
-
-function buscarTema(){
-  pageInicial = 0;
-  pageFinal = cantidadNoticias;
-
-  let tema = document.querySelector("#busqueda").value;
-  temaActual = tema;
-  noticias.fetchNoticias(temaActual);
-}
-
-function siguiente(){
-  pageInicial = pageFinal + 1;
-  pageFinal = pageFinal + cantidadNoticias + 1;
-  //eliminamos el botón siguiente
-  document.querySelector("#btnSiguiente").remove();
-  noticias.fetchNoticias(temaActual);
-
-}
-
-noticias.fetchNoticias(temaActual);
-
-
 require("dotenv").config();
 console.log("Client ID:", process.env.SPOTIFY_CLIENT_ID);
 console.log("Client Secret:", process.env.SPOTIFY_CLIENT_SECRET);
@@ -254,3 +158,98 @@ const cambiarTema=()=>{
     }
 }
 
+//API News
+let cantidadNoticias = 5;
+let pageFinal = cantidadNoticias;
+let pageInicial = 0;
+let temaActual = "Music";
+
+let noticias = {
+  "apiKey":"f3798e116eb342b2bae58e7f0cbd9c11",
+  fetchNoticias:function(temaActual){
+    fetch(
+      "https://newsapi.org/v2/everything?q="
+      +temaActual+
+      "&language=es&apiKey="+this.apiKey
+    )
+    .then((response)=>response.json())
+    .then((data)=>this.displayNoticias(data));
+  },
+  displayNoticias: function(data){
+    //elimino todo si ha seleccionado un nuevo tema
+    if(pageInicial==0){
+      document.querySelector(".container-noticias").textContent ="";
+    }
+
+    for(i=pageInicial;i<=pageFinal;i++){
+      const {title} = data.articles[i];
+      let h2 = document.createElement("h2");
+      h2.textContent = title;
+    
+      const {urlToImage} = data.articles[i];
+      let img = document.createElement("img");
+      img.setAttribute("src", urlToImage);
+
+      let info_item = document.createElement("div");
+      info_item.className = "info_item";
+      const {publishedAt} = data.articles[i];
+      let fecha = document.createElement("span");
+      let date = publishedAt;
+      date=date.split("T")[0].split("-").reverse().join("-");
+      fecha.className = "fecha";
+      fecha.textContent = date;
+
+      const {name} = data.articles[i].source;
+      let fuente = document.createElement("span");
+      fuente.className = "fuente";
+      fuente.textContent = name;
+
+      info_item.appendChild(fecha);
+      info_item.appendChild(fuente);
+
+      const {url} = data.articles[i];
+
+      let item = document.createElement("div");
+      item.className = "item";
+      item.appendChild(h2);
+      item.appendChild(img);
+      item.appendChild(info_item);
+      item.setAttribute("onclick", "location.href='"+url+"'");
+      document.querySelector(".container-noticias").appendChild(item);
+    }
+
+    let btnSiguiente = document.createElement("span");
+    btnSiguiente.id = "btnSiguiente";
+    btnSiguiente.textContent = "Ver más";
+    btnSiguiente.setAttribute("onclick","siguiente()");
+    document.querySelector(".container-noticias").appendChild(btnSiguiente);
+  }
+}
+
+
+function buscar(cat){
+  pageInicial = 0;
+  pageFinal = cantidadNoticias;
+  temaActual = cat;
+  noticias.fetchNoticias(cat);
+}
+
+function buscarTema(){
+  pageInicial = 0;
+  pageFinal = cantidadNoticias;
+
+  let tema = document.querySelector("#busqueda").value;
+  temaActual = tema;
+  noticias.fetchNoticias(temaActual);
+}
+
+function siguiente(){
+  pageInicial = pageFinal + 1;
+  pageFinal = pageFinal + cantidadNoticias + 1;
+  //eliminamos el botón siguiente
+  document.querySelector("#btnSiguiente").remove();
+  noticias.fetchNoticias(temaActual);
+
+}
+
+noticias.fetchNoticias(temaActual);
