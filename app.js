726fe2d7221b1a1dcf739016844fdd85fc9c7100
AxelelPro17 <amorales@siconta.com>
2025-11-22 23:13:19 -0600

    pequeño cambio ya pueden subir fotos menos de 5MB 51

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ const newsRoutes = require("./routes/news");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(express.static("public"));
 
 // Usa las rutas de Spotify y The News API
